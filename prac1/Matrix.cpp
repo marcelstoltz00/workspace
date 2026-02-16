ab1a42feb9bfdccfb7d268a247298cd659a762cf
marcelstoltz00 <marcelstoltz00@gmail.com>
2026-02-16 18:33:51 +0200

    Implement matrix multiplication and scalar multiplication in Matrix class

--- prac1/Matrix.cpp
+++ prac1/Matrix.cpp
@@ -51,10 +51,34 @@ Matrix<n, m> &Matrix<n, m>::operator=(const Matrix<n, m> &other) {
 
 template<int n, int m>
 template<int a>
-Matrix<n, a> Matrix<n, m>::operator*(const Matrix<m, a> &other) const { return Matrix<n, a>(); }
+Matrix<n, a> Matrix<n, m>::operator*(const Matrix<m, a> &other) const { 
+    Matrix<n, a> result;
+    for (int f = 0; f < n; f++) {
+        for (int s = 0; s < a; s++) {
+            float sum = 0.0f;
+            for (int i = 0; i < m; i++) {
+                sum += arr[f][i] * other.arr[i][s];
+            }
+            result.arr[f][s] = sum;
+        }
+    }
+    return result;  
+
+
+}
 
 template<int n, int m>
-Matrix<n, m> Matrix<n, m>::operator*(const float scalar) const { return Matrix<n, m>(); }
+Matrix<n, m> Matrix<n, m>::operator*(const float scalar) const { 
+    Matrix<n, m> result;
+    for (int f = 0; f < n; f++) {
+        for (int s = 0; s < m; s++) {
+            result.arr[f][s] = arr[f][s] * scalar;
+        }
+    }
+    return result;
+
+
+}
 
 template<int n, int m>
 Matrix<n, m> Matrix<n, m>::operator+(const Matrix<n, m> &other) const { 
@@ -68,7 +92,9 @@ Matrix<n, m> Matrix<n, m>::operator+(const Matrix<n, m> &other) const {
 }
 
 template<int n, int m>
-Matrix<m, n> Matrix<n, m>::operator~() const { return Matrix<m, n>(); }
+Matrix<m, n> Matrix<n, m>::operator~() const { 
+    
+ }
 
 
 
